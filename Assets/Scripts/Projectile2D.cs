b000f25c2f320644333049bbeb1220ab40593a3d
flup <naaom081@gmail.com>
2026-05-01 14:29:52 +0700

    Add Shooting script

--- Assets/Scripts/Projectile2D.cs
+++ Assets/Scripts/Projectile2D.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+using UnityEngine.InputSystem;
 
 public class Projectile2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D bulletPrefab;
     void Update()
     {
-        
+        Vector2 screenPos = Mouse.current.position.ReadValue();
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+            Ray ray = Camera.main.ScreenPointToRay(screenPos);
+            Debug.DrawRay(start: ray.origin, dir: ray.direction * 5f, Color.red, duration: 5f);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, distance: Mathf.Infinity);
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log($"Hit{hit.collider.gameObject.name}");
+
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+                shootBullet.linearVelocity = projectileVelocity;
+            }
+            Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+            {
+                Vector2 distance = target - origin;
+
+                float velocityX = distance.x / time;
+                float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+
+                Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+
+                return projectileVelocity;
+            }
+        }
     }
 }
